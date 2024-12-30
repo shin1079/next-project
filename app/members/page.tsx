80cbd701ddc2b5dd6d98b57dd70b595a03760153
shin1079 <d.0205.villa@gmail.com>
2024-12-30 21:33:21 +0900

    5章まで完了

--- app/members/page.tsx
+++ app/members/page.tsx
@@ -1,48 +1,10 @@
 import Image from "next/image";
+import { getMembersList } from "@/app/_libs/microcms";
+import { MEMBERS_LIST_LIMIT } from "@/app/_constants";
 import styles from "./page.module.css";
 
-const data = {
-  contents: [
-    {
-      id: "1",
-      image: {
-        url: "/img-member1.jpg",
-        width: 240,
-        height: 240,
-      },
-      name: "Aさん",
-      position: "CEO",
-      profile:
-        "ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。",
-    },
-    {
-      id: "2",
-      image: {
-        url: "/img-member2.jpg",
-        width: 240,
-        height: 240,
-      },
-      name: "Bさん",
-      position: "COO",
-      profile:
-        "ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。",
-    },
-    {
-      id: "3",
-      image: {
-        url: "/img-member3.jpg",
-        width: 240,
-        height: 240,
-      },
-      name: "Cさん",
-      position: "CTO",
-      profile:
-        "ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。",
-    },
-  ],
-};
-
-export default function Page() {
+export default async function Page() {
+  const data = await getMembersList({ limit: MEMBERS_LIST_LIMIT });
   return (
     <div className={styles.container}>
       {data.contents.length === 0 ? (
--- app/page.tsx
+++ app/page.tsx
@@ -1,53 +1,15 @@
 import styles from "./page.module.css";
 import Image from "next/image";
-
+import { getNewsList } from "@/app/_libs/microcms";
+import { TOP_NEWS_LIMIT } from "@/app/_constants";
 import NewsList from "@/app/_components/NewsList";
 import ButtonLink from "@/app/_components/ButtonLink";
-import { News } from "@/app/_libs/microcms";
 
-const data: { contents: News[]; } = {
-  contents: [
-    {
-      id: "1",
-      title: "記事01です！",
-      category: {
-        name: "カテゴリー01",
-      },
-      publishedAt: "2024/12/01",
-      createdAt: "2024/12/01",
-    },
-    {
-      id: "2",
-      title: "記事02です！",
-      category: {
-        name: "カテゴリー02",
-      },
-      publishedAt: "2024/12/02",
-      createdAt: "2024/12/02",
-    },
-    {
-      id: "3",
-      title: "記事03です！",
-      category: {
-        name: "カテゴリー03",
-      },
-      publishedAt: "2024/12/03",
-      createdAt: "2024/12/03",
-    },
-    {
-      id: "4",
-      title: "記事04です！",
-      category: {
-        name: "カテゴリー04",
-      },
-      publishedAt: "2024/12/04",
-      createdAt: "2024/12/04",
-    },
-  ],
-};
+export default async function Home() {
+  const data = await getNewsList({
+    limit: TOP_NEWS_LIMIT,
+  });
 
-export default function Home() {
-  const sliceData = data.contents.slice(0, 2);
   return (
     <>
       <section className={styles.top}>
@@ -62,7 +24,7 @@ export default function Home() {
 
       <section className={styles.news}>
         <h2 className={styles.newsTitle}>News</h2>
-        <NewsList news={sliceData}></NewsList>
+        <NewsList news={data.contents}></NewsList>
         <div className={styles.newsLink}>
           <ButtonLink href="/news">もっとみる</ButtonLink>
         </div>
